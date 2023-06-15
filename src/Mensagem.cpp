e97fd26355002d25f3f142430d210b6c9f4fa7c7
angelo-rbs <helenafranca1973@gmail.com>
2023-06-15 17:22:56 -0300

    implementação de Mensagem

---
+++ src/Mensagem.cpp
@@ -0,0 +1,53 @@
+#include "Mensagem.h"
+
+using std::string;
+
+// construtor
+
+Mensagem::Mensagem(string dataHora, int enviadaPor, string conteudo) {
+  this->dataHora = dataHora;
+  this->enviadaPor = enviadaPor;
+  this->conteudo = conteudo;
+}
+
+
+
+// getters e setters
+
+string Mensagem::getDataHora() {
+  return this->dataHora;
+} 
+
+int Mensagem::getEnviadaPor() {
+  return this->enviadaPor;
+}
+
+string Mensagem::getConteudo() {
+  return this->conteudo;
+}
+
+void Mensagem::setDataHora(string dataHora) {
+  this->dataHora = dataHora;
+} 
+
+void Mensagem::setEnviadaPor(int id) {
+
+  // checagem se o usuario existe no sistema
+
+  this->enviadaPor = id;
+}
+
+void Mensagem::setConteudo(string conteudo) {
+  this->conteudo = conteudo;
+}
+
+
+
+// sobrescrita de operadores
+
+bool Mensagem::operator==(Mensagem &msg) {
+  return this->dataHora == msg.getDataHora()
+      && this->conteudo == msg.getConteudo()
+      && this->enviadaPor == msg.getEnviadaPor();
+}
+
---
+++ src/Mensagem.h
@@ -0,0 +1,34 @@
+#ifndef MENSAGEM_H
+#define MENSAGEM_H
+
+#include <iostream>
+
+class Mensagem {
+
+  private:
+
+    std::string dataHora; // que tal usar uma entidade de Tempo do C++?
+    int enviadaPor;
+    std::string conteudo;
+
+  public:
+
+    // nao faz sentido criar uma mensagem vazia
+
+    Mensagem() = delete;  
+    Mensagem(std::string dataHora, int enviadaPor, std::string conteudo);
+
+    std::string getDataHora();
+    void setDataHora(std::string dataHora);
+
+    int getEnviadaPor();
+    void setEnviadaPor(int id);
+
+    std::string getConteudo();
+    void setConteudo(std::string conteudo);
+
+    bool operator==(Mensagem &mensagem);
+};
+
+
+#endif
