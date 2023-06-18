56c9893eaaf8e64c170ddc8e8f367642dc148515
angelo-rbs <helenafranca1973@gmail.com>
2023-06-18 15:31:13 -0300

    handler de exceções global na main

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include "Usuario.cpp"
 #include "Mensagem.cpp"
+#include "constants.cpp"
+#include "parser.cpp"
 
-int main() {
+int main()
+{
 
-  Mensagem *fst = new Mensagem("15/06/2023", 1, "oi tudo bem");
-  Mensagem *snd = new Mensagem("18/06/2025", 1, "tudo e com voce");
+  // Mensagem *fst = new Mensagem("15/06/2023", 1, "oi tudo bem");
+  // Mensagem *snd = new Mensagem("18/06/2025", 1, "tudo e com voce");
 
-  std::cout << fst->getDataHora() << ": " << fst->getConteudo() << ", enviada por " << fst->getEnviadaPor() << ";" << std::endl;
-  std::cout << snd->getDataHora() << ": " << snd->getConteudo() << ", enviada por " << snd->getEnviadaPor() << ";" << std::endl;
+  // std::cout << fst->getDataHora() << ": " << fst->getConteudo() << ", enviada por " << fst->getEnviadaPor() << ";" << std::endl;
+  // std::cout << snd->getDataHora() << ": " << snd->getConteudo() << ", enviada por " << snd->getEnviadaPor() << ";" << std::endl;
 
-  std::cout << (fst == snd) << std::endl;
+  // std::cout << (fst == snd) << std::endl;
 
-  delete fst;
-  delete snd;
+  // delete fst;
+  // delete snd;
+  try {
+    Parser *p = new Parser();
+    std::string in;
+
+    getline(std::cin, in);
+
+    p->parse(in);
+    p->print();
+
+  } catch (exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
 }
