22a11609288ca4000c105d77177fe79dc828a34e
angelo-rbs <helenafranca1973@gmail.com>
2023-06-19 15:46:56 -0300

    main de testes

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
+#include <exception>
+
+#include "Sistema.cpp"
 #include "Usuario.cpp"
 #include "Mensagem.cpp"
 #include "constants.cpp"
-#include "parser.cpp"
+#include "Parser.cpp"
 
 int main()
 {
 
-  // Mensagem *fst = new Mensagem("15/06/2023", 1, "oi tudo bem");
-  // Mensagem *snd = new Mensagem("18/06/2025", 1, "tudo e com voce");
-
-  // std::cout << fst->getDataHora() << ": " << fst->getConteudo() << ", enviada por " << fst->getEnviadaPor() << ";" << std::endl;
-  // std::cout << snd->getDataHora() << ": " << snd->getConteudo() << ", enviada por " << snd->getEnviadaPor() << ";" << std::endl;
-
-  // std::cout << (fst == snd) << std::endl;
-
-  // delete fst;
-  // delete snd;
   try {
-    Parser *p = new Parser();
-    std::string in;
+    
+    Sistema *sistema = new Sistema();
+    sistema->start();
 
-    getline(std::cin, in);
-
-    p->parse(in);
-    p->print();
+    delete sistema;
 
   } catch (exception &e) {
     std::cerr << e.what() << std::endl;
