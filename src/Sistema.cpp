b12d67b6b932ab0da253453b112b7ce6b00a9f55
angelo-rbs <helenafranca1973@gmail.com>
2023-06-19 15:46:47 -0300

    implementação de Sistema

--- src/Sistema.cpp
+++ src/Sistema.cpp
@@ -1,22 +1,157 @@
 #include "Sistema.h"
 
+#include "Parser.cpp"
+#include "constants.cpp"
+#include "dialogs.cpp"
 
-std::vector<Usuario> Sistema::getAllUsuarios() {
-  return this->usuarios;
+// variáveis estáticas
+
+int Sistema::idUsuarioLogado = cte::USUARIO_NAO_LOGADO;
+int Sistema::idCanalAtual = cte::CANAL_INDEFINIDO;
+int Sistema::idServidorAtual = cte::SERVIDOR_INDEFINIDO;
+
+// métodos de acesso
+
+std::vector<Usuario*> Sistema::getAllUsuarios() {
+    return this->usuarios;
 }
 
-std::vector<Servidor> Sistema::getAllServidores() {
-  return this->servidores;
+std::vector<Servidor*> Sistema::getAllServidores() {
+    return this->servidores;
 }
 
 int Sistema::getIdUsuarioLogado() {
-  return this->idUsuarioLogado;
+    return idUsuarioLogado;
 }
 
 int Sistema::getIdServidorAtual() {
-  return this->servidorAtual;
+    return idServidorAtual;
 }
 
 int Sistema::getIdCanalAtual() {
-  return this->canalAtual;
+    return idCanalAtual;
+}
+
+int Sistema::incrementAndGetIdUsuarioLogado() {
+    return Sistema::idUsuarioLogado++;
+}
+
+int Sistema::incrementAndGetServidorAtual() {
+    return Sistema::idServidorAtual++;
+}
+
+int Sistema::incrementAndGetCanalAtual() {
+    return Sistema::idCanalAtual++;
+}
+
+Usuario* Sistema::findUsuarioById(Usuario* user) {
+    for (auto usuario : usuarios)
+        if (usuario == user)
+            return usuario;
+
+    return nullptr;
+}
+
+Usuario* Sistema::findUsuarioByLogin(std::string email, std::string senha) {
+    for (auto usuario : usuarios)
+        if (usuario->getEmail() == email && usuario->getSenha() == senha)
+            return usuario;
+
+    return nullptr;
+}
+
+Usuario* Sistema::findUsuarioByEmail(std::string email) {
+    for (auto usuario : usuarios)
+        if (usuario->getEmail() == email)
+            return usuario;
+
+    return nullptr;
+}
+
+// funcionalidades
+
+void Sistema::quit() {
+    throw my_exit(0);
+}
+
+bool Sistema::createUser(std::string email, std::string senha, std::string nome) {
+    Usuario* user = new Usuario(nome, email, senha);
+
+    try {
+        if (findUsuarioByLogin(email, senha) != nullptr)
+            throw logic_error("já existe um usuário com este e-mail");
+
+        usuarios.push_back(user);
+        std::clog << "usuário criado com sucesso." << std::endl;
+        return true;
+    } catch (exception& e) {
+        std::cerr << "erro ao tentar criar usuário: " << e.what() << std::endl;
+        delete user;
+        return false;
+    }
+}
+
+bool Sistema::login(std::string email, std::string senha) {
+    if (Sistema::idUsuarioLogado != cte::USUARIO_NAO_LOGADO) {
+        std::clog << "já existe um usuário logado." << std::endl;
+        return false;
+    }
+
+    Usuario* user = new Usuario();
+
+    try {
+        user = findUsuarioByLogin(email, senha);
+
+        if (user != nullptr) {
+            idUsuarioLogado = user->getId();
+
+            std::cout << "usuário logado." << std::endl;
+            return true;
+        } else
+            std::cout << "usuário inexistente. verifique suas credenciais e "
+                         "tente novamente."
+                      << std::endl;
+
+        return false;
+    } catch (exception& e) {
+        std::cerr << "erro ao tentar login. verifique suas credenciais e tente "
+                     "novamente."
+                  << std::endl;
+        delete user;
+        return false;
+    }
+}
+
+void Sistema::start() {
+    try {
+        Parser* p = new Parser();
+        std::string commandLine, nome, email, senha;
+        bool parserOk;
+        int exit_flag = 0;
+
+        while (!exit_flag) {
+            getline(std::cin, commandLine);
+            if (!p->parse(commandLine))
+                continue;
+
+            if (p->getCommand() == cte::SAIR) {
+                quit();
+            } else if (p->getCommand() == cte::CRIAR_USUARIO) {
+                email = p->getArg(0);
+                senha = p->getArg(1);
+                nome = p->getArg(2);
+
+                createUser(email, senha, nome);
+            } else if (p->getCommand() == cte::LOGIN) {
+                email = p->getArg(0);
+                senha = p->getArg(1);
+
+                login(email, senha);
+            }
+        }
+
+        delete p;
+    } catch (Sistema::my_exit& exit) {
+        std::clog << "sessão Discordo encerrada." << std::endl;
+    }
 }
--- src/Sistema.h
+++ src/Sistema.h
@@ -3,28 +3,49 @@
 
 #include <iostream>
 #include <vector>
+#include <exception>
 
 #include "Usuario.h"
 #include "Servidor.h"
 #include "../infra/Loggable.cpp"
 
-class Sistema : Loggable {
+class Sistema : Loggable
+{
 
-  private:
-    std::vector<Usuario> usuarios;
-    std::vector<Servidor> servidores;
-    static int idUsuarioLogado;
-    static int servidorAtual;
-    static int canalAtual;
+private:
+  std::vector<Usuario*> usuarios;
+  std::vector<Servidor*> servidores;
+  static int idUsuarioLogado;
+  static int idServidorAtual;
+  static int idCanalAtual;
 
-  public:
-    std::vector<Usuario> getAllUsuarios();
-    std::vector<Servidor> getAllServidores();
-    int getIdUsuarioLogado();
-    int getIdServidorAtual();
-    int getIdCanalAtual();
+public:
+  Sistema() { }
 
+  struct my_exit : public std::exception
+  {
+    int value;
+    my_exit(int value) : value(value) {}
+  };
+
+  std::vector<Usuario*> getAllUsuarios();
+  std::vector<Servidor*> getAllServidores();
+  int getIdUsuarioLogado();
+  int getIdServidorAtual();
+  int getIdCanalAtual();
+  int incrementAndGetIdUsuarioLogado();
+  int incrementAndGetServidorAtual();
+  int incrementAndGetCanalAtual();
+
+  Servidor* findServidor(Servidor *server);
+  Usuario* findUsuarioById(Usuario *user);
+  Usuario* findUsuarioByLogin(std::string email, std::string senha);
+  Usuario* findUsuarioByEmail(std::string email);
+
+  void start();
+  void quit();
+  bool createUser(std::string email, std::string senha, std::string nome);
+  bool login(std::string email, std::string senha);
 };
-
 
 #endif
